481440e3c55712422f9be0dc03d8d33ea1a9db4b
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-05-28 13:46:21 +0900

    Groovy 파일 변경

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,52 +1,43 @@
-/* pipeline 변수 설정 */
-def app
+pipeline {
+    agent any
 
-node {
-    // gitlab으로부터 소스 다운하는 stage
-    stage('Checkout') {
-        checkout scm
-    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Building..."'
+                // 빌드 단계에서 수행할 작업을 추가할 수 있습니다.
+            }
+        }
 
-    // mvn 툴 선언하는 stage, 필자의 경우 maven 3.6.0을 사용중
-    stage('Ready'){
-        sh "echo 'Ready to build'"
-        mvnHome = tool 'Maven 3.6.0'
-    }
+        stage('Test') {
+            steps {
+                sh 'echo "Testing..."'
+                // 테스트 단계에서 수행할 작업을 추가할 수 있습니다.
+            }
+        }
 
-    // mvn 빌드로 jar파일을 생성하는 stage
-    stage('Build'){
-        sh "echo 'Build Spring Boot Jar'"
-        sh "'${mvnHome}/bin/mvn' clean package"
-    }
-
-    //sonarqube 정적분석 실행하는 stage, jenkins와 sonarqube연동을 하지 않았다면 이부분은 주석처리
-    stage('Static Code Analysis') {
-        sh "'${mvnHome}/bin/mvn' clean verify sonar:sonar -Dsonar.projectName=pipeline_test -Dsonar.projectKey=pipeline_test -Dsonar.projectVersion=$BUILD_NUMBER"
-    }
-
-    //dockerfile기반 빌드하는 stage ,git소스 root에 dockerfile이 있어야한다
-    stage('Build image'){
-        app = docker.build("cross9308/dockertest")
-    }
-
-
-    //docker image를 push하는 stage, 필자는 dockerhub에 이미지를 올렸으나 보통 private image repo를 별도 구축해서 사용하는것이 좋음
-    //docker.withRegistry에 dockerhub는 앞서 설정한 dockerhub credentials의 ID이다.
-    stage('Push image') {
-        docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-            app.push("${env.BUILD_NUMBER}")
-            app.push("latest")
+        stage('Deploy') {
+            steps {
+                sh 'echo "Deploying..."'
+                // 배포 단계에서 수행할 작업을 추가할 수 있습니다.
+            }
         }
     }
 
-    // kubernetes에 배포하는 stage, 배포할 yaml파일(필자의 경우 test.yaml)은 jenkinsfile과 마찬가지로 git소스 root에 위치시킨다.
-    // kubeconfigID에는 앞서 설정한 Kubernetes Credentials를 입력하고 'sh'는 쿠버네티스 클러스터에 원격으로 실행시킬 명령어를 기술한다.
-    stage('Kubernetes deploy') {
-        kubernetesDeploy configs: "test.yaml", kubeconfigId: 'Kuberconfig'
-        sh "/usr/local/bin/kubectl --kubeconfig=/u01/kube-config.yaml rollout restart deployment/test-deployment -n zuno"
-    }
+    post {
+        always {
+            sh 'echo "Post-build: Always"'
+            // 항상 수행되는 작업을 추가할 수 있습니다.
+        }
 
-    stage('Complete') {
-        sh "echo 'The end'"
+        success {
+            sh 'echo "Post-build: Success"'
+            // 성공적으로 완료된 경우 수행되는 작업을 추가할 수 있습니다.
+        }
+
+        failure {
+            sh 'echo "Post-build: Failure"'
+            // 실패한 경우 수행되는 작업을 추가할 수 있습니다.
+        }
     }
 }
